4293600d4b58027ee9a8f15d03fc2ab8c0f911a2
ChrisChandler325 <89486104+ChrisChandler325@users.noreply.github.com>
2023-07-10 20:22:38 -0500

    Add files via upload

---
+++ ChristopherChandlerM8/ChristopherChandlerM8/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Forms;
+
+
+namespace ChristopherChandlerM8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextDocument textdoc = new TextDocument();
+        Boolean modified = false;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+        }
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            if (handler())
+            {
+                textdoc.Open();
+                box.Text = textdoc.Text;
+                unsetModifiedState();
+            }
+        }
+        private Boolean handler()
+        {
+            if (!modified) return true;
+
+            string messageBoxText = "The text needs to be saved.\nDo you want to save?";
+            string caption = "Text Editor";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            // Display message box
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show(messageBoxText, caption, button, icon);
+            // Process message box results
+            switch (messageBoxResult)
+            {
+                case MessageBoxResult.Yes: // Save document and exit
+                    //startSave();
+                    return true;
+
+                case MessageBoxResult.No: // Exit without saving
+                    return true;
+
+                case MessageBoxResult.Cancel: // Don't exit
+                    return false;
+            }
+            return false;
+        }
+        private void about(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "This is a text editor made by Christopher Chandler with a pawprint of cjc7nw.";
+            string caption = "About";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            // Display message box
+            System.Windows.MessageBox.Show(messageBoxText, caption, button, icon);
+            // No need to process message box results since OK is the only option.
+        }
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            if (handler())
+            {
+                Close();
+            }
+        }
+        private void txtNew(object sender, RoutedEventArgs e)
+        {
+            if (handler())
+            {
+                textdoc.New();
+                box.Text = textdoc.Text;
+                unsetModifiedState();
+            }
+        }
+        private void saveasTxt(object sender, RoutedEventArgs e)
+        {
+            textdoc.startSave();
+            unsetModifiedState();
+        }
+        private void savetxt(object sender, RoutedEventArgs e)
+        {
+            textdoc.saveDoc();
+            unsetModifiedState();
+        }
+        private void textChanged(Object sender, RoutedEventArgs e)
+        {
+            textdoc.Text = box.Text;
+            setModifiedState();
+        } 
+        private void setModifiedState()
+        {
+            modified = true;
+            save.IsEnabled = true;
+        }
+        private void unsetModifiedState()
+        {
+            modified = false;
+            save.IsEnabled = false;
+        }
+    }
+}
---
+++ ChristopherChandlerM8/ChristopherChandlerM8/TextDocument.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ChristopherChandlerM8
+{
+    [Serializable]
+
+    //open and save text file methods
+    class TextDocument
+    {
+        
+        public TextDocument() 
+        { 
+        }
+        public String Text { get; set; }
+        public String FilePath { get; set; }
+
+        public Boolean Load()
+        {
+            this.Text = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(this.FilePath))
+                {
+                    String line = "";
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        this.Text += line;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error",ex.Message);
+                return false;
+            }
+
+            
+        }
+        public void New() 
+        {
+            this.Text = "";
+        }
+        public void Open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "txt files (*.txt) |*.txt";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.FilePath= openFileDialog.FileName;
+                this.Load();
+            }
+        }
+        public void startSave()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // http://msdn.microsoft.com/en-us/library/system.io.path.getdirectoryname.aspx
+
+            if (this.FilePath != "")
+            {
+                saveFileDialog.FileName = System.IO.Path.GetFileName(this.FilePath);
+                saveFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(this.FilePath);
+            }
+
+            saveFileDialog.DefaultExt = ".txt"; // Default file extension
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt"; // Filter files by extension
+            // Show save file dialog box
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.FilePath = saveFileDialog.FileName;
+                saveDoc();
+            }
+        }
+        public void saveDoc()
+        {
+            var currentTxt = new TextDocument();
+            if (this.Text != null)
+            {
+                currentTxt.Text = this.Text.ToString();
+            }
+            else
+            {
+                currentTxt.Text = " ";
+            }
+            
+            currentTxt.FilePath = this.FilePath;
+            if (currentTxt.FilePath != null)
+            {
+                try
+                {
+                    var fStreamTxt = new FileStream(FilePath, FileMode.Create);
+                    using (StreamWriter s = new StreamWriter(fStreamTxt))
+                    {
+                        s.Write(currentTxt.Text);
+                    }
+                    fStreamTxt.Close();
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            else
+            {
+                startSave();
+            }
+            this.FilePath= currentTxt.FilePath;
+        }
+    }
+}
